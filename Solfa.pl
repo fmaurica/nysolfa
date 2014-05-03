9a4670caae0427be6a16aec5bec9bebcf34bb49e
MAURICA A. Fonenantsoa (fmaurica) <maurica.fonenantsoa@gmail.com>
2014-05-03 11:24:23 +0400

    sf format fixed

--- Solfa.pl
+++ Solfa.pl
@@ -5,8 +5,12 @@ package Solfa;
 use Exporter qw(import);
 our @EXPORT_OK = qw(sf2ly);
 
+use Data::Dumper qw(Dumper);
+
 use constant FIDIR => ".";
 use constant LYTPLFINAME => ".tmpl.ly";
+
+use constant STEP => 2;
 
 $ENV{PATH}='C:\Program Files\Intel\iCLS Client\;%SystemRoot%\system32;%SystemRoot%;%SystemRoot%\System32\Wbem;%SYSTEMROOT%\System32\WindowsPowerShell\v1.0\;C:\Program Files\Intel\OpenCL SDK\2.0\bin\x86;C:\Program Files\Intel\Intel(R) Management Engine Components\DAL;C:\Program Files\Intel\Intel(R) Management Engine Components\IPT;C:\Program Files\Intel\WiFi\bin\;C:\Program Files\Common Files\Intel\WirelessCommon\;C:\Program Files\QuickTime\QTSystem\;C:\strawberry\c\bin;C:\strawberry\perl\site\bin;C:\strawberry\perl\bin;C:\Program Files\LilyPond\usr\bin';
 
@@ -20,14 +24,34 @@ sub sf2ly {
   my $sflines;
   while(<SFFI>){ $sflines .= $_; }
   close(SFFI);
+  
+  # getting meta info
   if($sflines =~ /Hira:(.*)\n/){ $scoremeta{'piece'} = trim($1); }
   if($sflines =~ /Namorona ny feony:(.*)\n/){ $scoremeta{'composer'} = trim($1); }
   if($sflines =~ /Nandika ny feony:(.*)\n/){ $scoremeta{'arranger'} = trim($1); }
   if($sflines =~ /Tonony:(.*)\n/){ $scoremeta{'poet'} = trim($1); }
-  $sflines =~ s/\n/%/g;
-  #print "$sflines";
-  my @staffs = split /%{3,}/,$sflines;
-  print "@staffs\n";
+
+  # getting staffs
+  my %score = ('voiceOneUp','voiceOne','voiceTwo','voiceThree','voiceFour');
+  my @staffs = split /#\n/,$sflines;
+  foreach my $i (1 .. $#staffs) {
+	my @voices = split /\n/,$staffs[$i];	
+    print Dumper @voices;
+
+	my $index = 0;
+	while ($index <= length(@voices[1])) {
+	  my $noteOneTmp = substr (@voices[2], $index, 2);
+	  print "$index: $noteOneTmp\n";
+	  $index = $index + STEP + 2;
+	}
+#	print "@voices[0]\n";
+#	print "@voices[1]\n";
+#	print "@voices[2]\n";
+#	print "@voices[3]\n";
+#	print "@voices[4]\n";
+#	print "@voices[5]\n";
+	print "\n";
+  }
   
   open(LYTPLFI,FIDIR."/".LYTPLFINAME);
   my $lytpllines;
@@ -56,5 +80,6 @@ sub trim {
    return $_[0] =~ s/^\s+|\s+$//rg;
 }
 
+print "Content-type: text/html\n\n";
 &sf2ly("C:\\Program Files\\LightTPD\\htdocs\\solfa\\fihirana-ffpm_441.sf");
 1;
